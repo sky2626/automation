4ebd0d22ec5d877f2094f842a2fb33a15a280ed0
sky2626 <alhassansalu1@gmail.com>
2023-09-29 00:44:38 -1200

    update

---
+++ git/auto.py
@@ -0,0 +1,20 @@
+import schedule
+import time
+import subprocess
+
+def push_code():
+    print(f'Pushing code to git repository...')
+    try:
+        # Run git script
+        subprocess.run(["python", "script.py"])
+        print("Code push successfully.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+# time to run
+schedule.every(10).seconds.do(push_code)
+
+# schedule continue
+while True:
+    schedule.run_pending()
+    time.sleep(1)
